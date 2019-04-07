dcc0de85c49335b740dab9e800b3f7ff4aa4a8a3
Travis Shears <btbtravis@gmail.com>
2019-04-07 20:45:14 +0200

    Init postgres dev connection

--- config/dev.exs
+++ config/dev.exs
@@ -60,8 +60,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :click_game, ClickGame.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "devpostgres",
+  password: "click",
   database: "click_game_dev",
   hostname: "localhost",
   pool_size: 10
